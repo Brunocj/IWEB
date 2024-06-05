2daa458feb3bde4e1a9906bd7ba8a11eec4dd307
Brunocj <a20212474@pucp.edu.pe>
2024-06-05 14:28:51 -0500

    DANIEL

--- src/main/java/org/example/webappsm/Servlet/admin/AdminServlet.java
+++ src/main/java/org/example/webappsm/Servlet/admin/AdminServlet.java
@@ -6,7 +6,6 @@ import jakarta.servlet.annotation.*;
 import org.example.webappsm.model.beans.Profesor;
 import org.example.webappsm.model.beans.Serenazgo;
 import org.example.webappsm.model.beans.Usuario;
-import org.example.webappsm.model.daos.DashboardDao;
 import org.example.webappsm.model.daos.ProfesorDao;
 import org.example.webappsm.model.daos.SerenazgoDao;
 import org.example.webappsm.model.daos.VecinosDao;
@@ -14,10 +13,8 @@ import org.example.webappsm.model.daos.VecinosDao;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.*;
-import java.util.HashSet;
-import java.util.Set;
-
+import java.util.ArrayList;
+import java.util.Date;
 @WebServlet(name = "AdminServlet", value = "/Admin")
 public class AdminServlet extends HttpServlet {
 
@@ -35,23 +32,6 @@ public class AdminServlet extends HttpServlet {
                 rd.forward(request,response);
                 break;
             case "dashboard":
-                DashboardDao dashboardDao = new DashboardDao();
-                String totalBaneados = dashboardDao.totalBaneados();
-                String avgIncidencias = dashboardDao.avgIncidencias();
-                String totalIncidencias = dashboardDao.totalIncidencias();
-                String incidenciasComunMax = dashboardDao.incidenciaComunMax();
-                String incidenciasComunMin = dashboardDao.incidenciaComunMin();
-                String incidenciasPorAtender = dashboardDao.incidenciasPorAtender();
-                String incidenciasUrbMax = dashboardDao.incidenciasUrbMax();
-                String incidenciasUrbMin = dashboardDao.incidenciasUrbMin();
-                request.setAttribute("totalbaneados", totalBaneados);
-                request.setAttribute("avgincidencias", avgIncidencias);
-                request.setAttribute("totalincidencias", totalIncidencias);
-                request.setAttribute("incidenciascomunmax", incidenciasComunMax);
-                request.setAttribute("incidenciascomunmin",incidenciasComunMin);
-                request.setAttribute("incidenciasatender", incidenciasPorAtender);
-                request.setAttribute("incidenciasurbmax",incidenciasUrbMax);
-                request.setAttribute("incidenciasurbmin", incidenciasUrbMin);
                 vista = "vistas/jsp/ADMIN/Dashboard/dashboard.jsp";
                 rd = request.getRequestDispatcher(vista);
                 rd.forward(request,response);
@@ -92,22 +72,8 @@ public class AdminServlet extends HttpServlet {
                 ProfesorDao profesorDao = new ProfesorDao();
                 ArrayList<Profesor> list = profesorDao.listarProfesoresTabla();
 
-                Set<String> cursosUnicos = new HashSet<>();
-                Set<String> areasUnicas = new HashSet<>();
-
-                for (Profesor profesor : list) {
-                    cursosUnicos.add(profesor.getCurso());
-                    areasUnicas.add(profesor.getNombreArea());
-                }
                 //OBJETO A ENVIAR
-                //OBJETO A ENVIAR
-                List<String> listaCursos = new ArrayList<>(cursosUnicos);
-                List<String> listaAreas = new ArrayList<>(areasUnicas);
-
-
                 request.setAttribute("listaprofesor",list);
-                request.setAttribute("listaCursos", listaCursos);
-                request.setAttribute("listaAreas", listaAreas);
 
                 vista = "vistas/jsp/ADMIN/Profesores/tabla_profesor.jsp";
                 rd = request.getRequestDispatcher(vista);
@@ -136,7 +102,7 @@ public class AdminServlet extends HttpServlet {
                     response.sendError(HttpServletResponse.SC_BAD_REQUEST, "ID del profesor no es válido");
                 }
                 break;
-            case "tablaAcceso":
+             case "tablaAcceso":
                 VecinosDao vecinosDao = new VecinosDao();
                 ArrayList<Usuario> listaAcceso = vecinosDao.listarSoliAcceso();
 
@@ -298,14 +264,14 @@ public class AdminServlet extends HttpServlet {
         }else if(action.equals("eliminarSerenazgo")){
 
             String idParam = request.getParameter("id");
-            if (idParam != null) {
-                int idSerenazgo = Integer.parseInt(idParam);
+                if (idParam != null) {
+                    int idSerenazgo = Integer.parseInt(idParam);
 
-                SerenazgoDao serenazgoDao = new SerenazgoDao();
-                serenazgoDao.eliminarSerenazgo(idSerenazgo);
+                    SerenazgoDao serenazgoDao = new SerenazgoDao();
+                    serenazgoDao.eliminarSerenazgo(idSerenazgo);
 
-                response.sendRedirect(request.getContextPath() + "/Admin?action=tablaSerenazgo");
-            }
+                    response.sendRedirect(request.getContextPath() + "/Admin?action=tablaSerenazgo");
+                }
         }else{
             response.sendRedirect(request.getContextPath() + "/Admin");
         }
