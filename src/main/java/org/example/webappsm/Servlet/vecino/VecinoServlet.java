db9e825e362a19de883c9556165f262ed897d0d4
Brunocj <a20212474@pucp.edu.pe>
2024-06-10 17:57:37 -0500

    Servlet creado

---
+++ src/main/java/org/example/webappsm/Servlet/vecino/VecinoServlet.java
@@ -0,0 +1,35 @@
+package org.example.webappsm.Servlet.vecino;
+
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+import java.io.IOException;
+
+@WebServlet(name ="VecinoServlet" , value = "/Vecino")
+public class VecinoServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action") == null? "pagPrincipal" : request.getParameter("action");
+        String vista;
+        RequestDispatcher rd ;
+        switch (action){
+            case "pagPrincipal":
+                vista = "vistas/jsp/VECINO/pagina_principal_vecino.jsp";
+                rd = request.getRequestDispatcher(vista);
+                rd.forward(request,response);
+                break;
+
+        }
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    }
+}
