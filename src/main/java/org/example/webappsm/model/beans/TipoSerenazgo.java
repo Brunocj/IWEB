09e42d636b13a3094eee3d7767fb15a73062bf39
DanielGarayC <148827790+DanielGarayC@users.noreply.github.com>
2024-06-12 01:14:55 -0500

    Create TipoSerenazgo.java

---
+++ src/main/java/org/example/webappsm/model/beans/TipoSerenazgo.java
@@ -0,0 +1,22 @@
+package org.example.webappsm.model.beans;
+
+public class TipoSerenazgo {
+    private int idTipoSerenazgo;
+    private String nombreTipo;
+
+    public int getIdTipoSerenazgo() {
+        return idTipoSerenazgo;
+    }
+
+    public void setIdTipoSerenazgo(int idTipoSerenazgo) {
+        this.idTipoSerenazgo = idTipoSerenazgo;
+    }
+
+    public String getNombreTipo() {
+        return nombreTipo;
+    }
+
+    public void setNombreTipo(String nombreTipo) {
+        this.nombreTipo = nombreTipo;
+    }
+}
