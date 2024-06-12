a6d8d4d262549743a38877d1eee421ae300d0ba7
DanielGarayC <148827790+DanielGarayC@users.noreply.github.com>
2024-06-12 11:29:29 -0500

    Update VecinosDao.java

--- src/main/java/org/example/webappsm/model/daos/VecinosDao.java
+++ src/main/java/org/example/webappsm/model/daos/VecinosDao.java
@@ -20,7 +20,7 @@ public class VecinosDao {
         //Parametros de conexion a que base de datos me quiero unir//
         String url ="jdbc:mysql://localhost:3306/sanmiguel";
         String username = "root";
-        String password = "123456";
+        String password = "root";
         String sql = "SELECT u.idUsuario, u.nombres, u.apellidos, e.nombreEstado AS estado " +
                 "FROM Usuario u " +
                 "JOIN Estado e ON u.Estado_idEstado = e.idEstado " +
@@ -61,7 +61,7 @@ public class VecinosDao {
         //Parametros de conexion a que base de datos me quiero unir//
         String url ="jdbc:mysql://localhost:3306/sanmiguel";
         String username = "root";
-        String password = "123456";
+        String password = "root";
         String sql = "SELECT " +
                 "u.idUsuario," +
                 "u.nombres, " +
@@ -95,4 +95,110 @@ public class VecinosDao {
 
         return listaCoord;
     }
+
+    public void darAcceso(){
+
+        try {
+            Class.forName( "com.mysql.cj.jdbc.Driver");
+
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        String url ="jdbc:mysql://localhost:3306/sanmiguel";
+        String username = "root";
+        String password = "root";
+
+    }
+
+    public Usuario obtenerUsuarioPorId(int id) {
+        Usuario usuario = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        String url = "jdbc:mysql://localhost:3306/sanmiguel";
+        String username = "root";
+        String password = "root";
+
+        String sql = "SELECT * FROM Usuario WHERE idUsuario = ?";
+
+        try (Connection conn = DriverManager.getConnection(url, username, password);
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setInt(1, id);
+            try (ResultSet rs = pstmt.executeQuery()) {
+                if (rs.next()) {
+                    usuario = new Usuario();
+                    usuario .setNombre(rs.getString("nombres"));
+                    usuario .setApellido(rs.getString("apellidos"));
+                    usuario .setDocumento(rs.getString("nroDocumento"));
+                    usuario .setDireccion(rs.getString("direccion"));
+                    usuario .setDistrito(rs.getString("distrito"));
+                    usuario .setUrbanizacion(rs.getString(8));
+                    usuario .setCorreoE(rs.getString("correo"));
+
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return usuario;
+    }
+    public void aprobarSol(int idUsuario){
+        try {
+            Class.forName( "com.mysql.cj.jdbc.Driver");
+
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        String url ="jdbc:mysql://localhost:3306/sanmiguel";
+        String username = "root";
+        String password = "root";
+        String query = "DELETE FROM Usuario WHERE idUsuario = ?";
+        try (Connection conn= DriverManager.getConnection(url, username, password);
+             PreparedStatement pstmt = conn.prepareStatement(query)){
+            pstmt.setInt(1, idUsuario);
+            pstmt.executeUpdate();
+        }catch( SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void borrar_vecino(int id_usuario, int opcion_boton){
+
+        try {
+            Class.forName( "com.mysql.cj.jdbc.Driver");
+
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        String url ="jdbc:mysql://localhost:3306/sanmiguel";
+        String username = "root";
+        String password = "root";
+
+
+        if (opcion_boton == 1){
+            String query="";
+        } else if (opcion_boton == 2){
+            String query="";
+        }
+
+        String query = "DELETE FROM Usuario";
+        try (Connection conn= DriverManager.getConnection(url, username, password);
+             PreparedStatement pstmt = conn.prepareStatement(query)){
+
+
+        }catch( SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
+
 }
