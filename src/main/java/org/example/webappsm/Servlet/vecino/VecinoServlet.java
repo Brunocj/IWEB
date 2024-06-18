0f726794700c987c750248abe42001c2b85f6309
Brunocj <a20212474@pucp.edu.pe>
2024-06-18 00:40:36 -0500

    incidencias(vevino)

--- src/main/java/org/example/webappsm/Servlet/vecino/VecinoServlet.java
+++ src/main/java/org/example/webappsm/Servlet/vecino/VecinoServlet.java
@@ -3,19 +3,29 @@ package org.example.webappsm.Servlet.vecino;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.MultipartConfig;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.example.webappsm.model.beans.Incidencia;
+import org.example.webappsm.model.beans.Tipos;
+import org.example.webappsm.model.beans.Urbanizacion;
 import org.example.webappsm.model.beans.Usuario;
 import org.example.webappsm.model.daos.UserDao;
 
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.text.ParseException;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 @WebServlet(name ="VecinoServlet" , value = "/Vecino")
+@MultipartConfig
 public class VecinoServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -58,6 +68,24 @@ public class VecinoServlet extends HttpServlet {
                 rd = request.getRequestDispatcher(vista);
                 rd.forward(request,response);
                 break;
+            case "registroIncidencia":
+                ArrayList<Urbanizacion> listaUrbanizaciones = userDao.listarUrbanizaciones();
+                ArrayList<Tipos> tiposIncidencias = userDao.listarTiposIncidencias();
+                request.setAttribute("listaUrbanizaciones", listaUrbanizaciones);
+                request.setAttribute("tiposIncidencias", tiposIncidencias);
+                vista = "vistas/jsp/VECINO/incidencias/registrar_incidencia.jsp";
+                rd = request.getRequestDispatcher(vista);
+                rd.forward(request,response);
+                break;
+            case "infoIncidencia":
+                int idIncidencia = Integer.parseInt(request.getParameter("idIncidencia"));
+                Incidencia incidencia = userDao.getIncidenciaId(idIncidencia);
+                request.setAttribute("incidencia", incidencia);
+                vista = "vistas/jsp/VECINO/incidencias/incidencia_info.jsp";
+                rd = request.getRequestDispatcher(vista);
+                rd.forward(request,response);
+                break;
+
 
         }
 //xd
@@ -75,7 +103,54 @@ public class VecinoServlet extends HttpServlet {
                 userDao.enviarSolicitud(idUsuario, idArea);
                 response.sendRedirect(request.getContextPath() + "/Vecino?action=solCoordinador");
                 break;
+            case "registrarIncidencia":
+                Incidencia incidencia = new Incidencia();
+                String nombreIncidencia = request.getParameter("nombreIncidencia");
+                String lugar = request.getParameter("lugar");
+                String referencia = request.getParameter("ref");
+                int idUrbanizacion = Integer.parseInt(request.getParameter("idUrb"));
+                String contacto = request.getParameter("contacto");
+                Boolean necesitaAmbulancia = Boolean.parseBoolean(request.getParameter("ambulanciaSN"));
+                byte[] evidencia = obtenerImagenComoByteArray((request.getPart("foto").getInputStream()));
+                String fechaStr = request.getParameter("fecha");
+
+                try {
+                    // Definir el formato de la fecha/hora
+                    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                    // Parsear la cadena a un objeto java.util.Date
+                    Date fechaUtil = sdf.parse(fechaStr);
+                    // Convertir el objeto java.util.Date a java.sql.Timestamp
+                    Timestamp fechaSql = new Timestamp(fechaUtil.getTime());
+                    incidencia.setFechaIncidencia(fechaSql);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+
+                int idTipo = Integer.parseInt(request.getParameter("idTipoIncidencia"));
+                int idUsuarioIncidencia = Integer.parseInt(request.getParameter("idUsuario"));
+                incidencia.setNombre(nombreIncidencia);
+                incidencia.setLugar(lugar);
+                incidencia.setReferencia(referencia);
+                incidencia.setIdUrbanizacion(idUrbanizacion);
+                incidencia.setContactoO(contacto);
+                incidencia.setAmbulanciaI(necesitaAmbulancia);
+                incidencia.setImgEvidencia(evidencia);
+                incidencia.setIdTipo(idTipo);
+                incidencia.setIdUsuario(idUsuarioIncidencia);
+                userDao.agregarIncidencia(incidencia);
+                response.sendRedirect(request.getContextPath() + "/Vecino?action=incidencias");
+                break;
         }
 
+    }
+    public static byte[] obtenerImagenComoByteArray(InputStream inputStream) throws IOException {
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        int nRead;
+        byte[] data = new byte[1024];
+        while ((nRead = inputStream.read(data, 0, data.length)) != -1) {
+            buffer.write(data, 0, nRead);
+        }
+        buffer.flush();
+        return buffer.toByteArray();
     }
 }
